c6c68cc43f285fb30615f111d9b08128647eac95
Roykesydon <roy23381547@gmail.com>
2024-01-11 00:28:26 +0800

    feat: remove button ui after select

--- core/commands/channel.py
+++ core/commands/channel.py
@@ -36,15 +36,13 @@ class ChannelCommand(commands.Cog):
 
             async def enable_callback(interaction):
                 Validator.enable_channel(interaction.channel.id)
-                await interaction.response.send_message(
-                    CHANNEL_TEXT_DICT["enable-response"]
-                )
+                await interaction.response.edit_message(view=None)
+                await interaction.followup.send(CHANNEL_TEXT_DICT["enable-response"])
 
             async def disable_callback(interaction):
                 Validator.disable_channel(interaction.channel.id)
-                await interaction.response.send_message(
-                    CHANNEL_TEXT_DICT["disable-response"]
-                )
+                await interaction.response.edit_message(view=None)
+                await interaction.followup.send(CHANNEL_TEXT_DICT["disable-response"])
 
             # show current status
             enabled = interaction.channel.id in Validator.get_enabled_channels()
