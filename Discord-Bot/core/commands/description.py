7ad4f0c3f04a6bf4df370e662039cae49f5240ba
Roykesydon <roy23381547@gmail.com>
2024-01-16 05:59:27 +0800

    feat: show description about this robot

---
+++ Discord-Bot/core/commands/description.py
@@ -0,0 +1,27 @@
+import discord
+from core.config import LANG_DATA
+from discord import app_commands
+from discord.ext import commands
+
+
+class DescriptionCommand(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @app_commands.command(
+        name="description",
+        description=LANG_DATA["commands"]["description"]["description"],
+    )
+    async def show_description(self, interaction):
+        async with interaction.channel.typing():
+            # send logo
+            logo = discord.File("./assets/logo.png")
+            await interaction.channel.send(file=logo)
+
+            # send description
+            description = LANG_DATA["description"]
+            await interaction.channel.send(description)
+
+
+async def setup(bot):
+    await bot.add_cog(DescriptionCommand(bot))
