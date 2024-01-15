cf66ffdb0ca589f25e96a5b9ab4e63c398e35c80
Roykesydon <roy23381547@gmail.com>
2024-01-15 22:29:39 +0800

    feat: formatting script

---
+++ format.py
@@ -0,0 +1,10 @@
+import os
+
+# pyright
+os.system("pyright .")  # statically type checking
+
+# black
+os.system("black .")  # coding style formatting
+
+# isort
+os.system("isort .")  # import sorting
