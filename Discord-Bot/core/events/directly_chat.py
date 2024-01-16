3e35c0ef49f602d56783b8ff99740ef5754a01f7
Roykesydon <roy23381547@gmail.com>
2024-01-16 09:01:29 +0800

    feat: direct message to bot without any prefix command

---
+++ Discord-Bot/core/events/directly_chat.py
@@ -0,0 +1,29 @@
+import discord
+from discord.ext import commands
+
+
+class DirectlyChat(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_message(self, message: discord.Message):
+        # Ignore message from bot
+        if message.author == self.bot.user:
+            return
+
+        # Ignore message that start with /
+        if message.content.startswith("/"):
+            return
+
+        async with message.channel.typing():
+            """
+            TODO: get response from chatbot
+            """
+            chatbot_message = "測試用 LLM 回覆訊息"
+
+        return await message.channel.send(chatbot_message)
+
+
+async def setup(bot):
+    await bot.add_cog(DirectlyChat(bot))
