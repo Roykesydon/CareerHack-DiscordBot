81140f4fa0ac3e37d712c7050f43a88279fbaad2
Roykesydon <roy23381547@gmail.com>
2024-01-16 11:36:22 +0800

    feat: supply discord embed formay

---
+++ Discord-Bot/core/message.py
@@ -0,0 +1,28 @@
+import discord
+from core.config import CONFIG
+
+
+class Message:
+    def __init__(self, text=""):
+        self._text = text
+
+    def get_embed_format(
+        self, title: str, color: str = CONFIG["primary_color"]
+    ) -> discord.Embed:
+        # set embed color
+        color_int = int(color.replace("#", ""), 16)
+
+        # send commands list
+        return discord.Embed(
+            title=title,
+            description=self.get_text(),
+            color=color_int,
+        )
+
+    # getters and setters
+
+    def get_text(self) -> str:
+        return self._text
+
+    def set_text(self, text: str):
+        self._text = text
