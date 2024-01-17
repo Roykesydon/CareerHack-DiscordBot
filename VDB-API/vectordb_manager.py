d2a685f803a33e666c8c15c6e75bd674fd8ea02c
charlie <123charlie200@gmail.com>
2024-01-17 21:51:28 +0800

    feat: return document content and information

--- VDB-API/vectordb_manager.py
+++ VDB-API/vectordb_manager.py
@@ -7,6 +7,7 @@ from langchain_community.embeddings.sentence_transformer import (
 
 from utils.config import CONFIG
 
+
 class VectordbManager:
     _emb_fn = SentenceTransformerEmbeddings(
         model_name=CONFIG["HuggingFace"]["model_name"]
@@ -20,11 +21,11 @@ class VectordbManager:
             client=VectordbManager._chroma_client,
             embedding_function=VectordbManager._emb_fn,
             persist_directory=CONFIG["db_path"],
-            collection_name=CONFIG["collections"]["basic"]
+            collection_name=CONFIG["collections"]["basic"],
         )
 
     # 切換 collection，若不存在則自動創建
-    def set_vector_db(self, collection_name: str):
+    def set_vector_db(self, collection_name):
         self.vectordb = Chroma(
             client=VectordbManager._chroma_client,
             embedding_function=VectordbManager._emb_fn,
@@ -34,73 +35,97 @@ class VectordbManager:
         print("set collection to", collection_name)
 
     # 回傳所有的 collection name
-    def get_collection_name_list(self) -> list[str]:
+    def get_collection_name_list(self):
         collection_list = VectordbManager._chroma_client.list_collections()
         name_list = [collection.name for collection in collection_list]
         return name_list
 
     # 回傳所有 collection 的名稱
-    def get_current_collection_name(self) -> str:
+    def get_current_collection_name(self):
         return self.vectordb._collection.name
 
     # 儲存切割後的資料
-    def add(self, texts: list[Document]) -> list[str]:
+    def add(self, texts):
+        """
+        Args:
+            texts: split data from document.
+                type: list[Document]
+                *note: you can use file_processor.get_split_data(file_path) to get split data
+        Returns:
+            ids: id of each document in vector database
+        """
         print("adding data...")
         ids = self.vectordb.add_documents(texts)
         self.vectordb.persist()  # ensure the embeddings are written to disk
         return ids
 
     # 獲取指定條件的 document (文件段落)
-    def get(self, where: dict[str]) -> list[str]:
-        """Args example:
-        where =
-            (single condition)   {"source": "pdf-1"}
-            (multiple condition) {"$or": [{"source": "pdf-1"}, {"source": "pdf-4"}]}
+    def get(self, where):
         """
-        retrieved_data = self.vectordb._collection.get(where=where)
-        retrieved_text = (
-            retrieved_data["documents"]
-            if len(retrieved_data["documents"]) > 0
-            else []
-        )
-        return retrieved_text
-    
+        Args example:
+            where =
+                (single condition)   {"source": "pdf-1"}
+                (multiple condition) {"$or": [{"source": "pdf-1"}, {"source": "pdf-4"}]}
+        Returns:
+            contents: list of document's page_content. list[str]
+            metadatas: list of document's infomation. list[dict[str]]
+                [{
+                    'source': 段落來源檔名(str),
+                    'page': 段落所在頁碼(int) # 只有 pdf 才有
+                    }, {}, ...
+                ]
+        """
+        docs = self.vectordb._collection.get(where=where)
+        if len(docs["documents"]) > 0:
+            contents, metadatas = [], []
+            for i, doc in enumerate(docs["documents"]):
+                contents.append(doc)
+                metadatas.append(docs["metadatas"][i])
+            return contents, metadatas
+        else:
+            print("No data found")
+            return [], []
+
     # 獲取 collection 中所有資料的來源檔名
-    def get_all_source_name(self) -> list[str]:
+    def get_all_source_name(self):
         all_data = self.vectordb._collection.get()
-        source_name_list = [data['source'] for data in all_data['metadatas']]
+        source_name_list = [data["source"] for data in all_data["metadatas"]]
         source_name_list = list(set(source_name_list))  # Remove duplicates
         return source_name_list
 
     # 回傳與問題相關的資料
-    def query(self, query, n_results=1, where=None) -> list[Document]:
+    def query(self, query, n_results=1, where=None):
         """
-            Args example:
-                where =
-                    (single condition)   {"source": "pdf-1"}
-                    (multiple condition) {"$or": [{"source": "pdf-1"}, {"source": "pdf-4"}]}
-            Returns:
-                Document(
-                    page_content: 文章段落,
-                    metadata: {
-                        'page': 段落所在頁碼(int),
-                        'source':  段落來源檔名(str)
-                    }
-                )       
+        Args example:
+            where =
+                (single condition)   {"source": "pdf-1"}
+                (multiple condition) {"$or": [{"source": "pdf-1"}, {"source": "pdf-4"}]}
+        Returns:
+            contents: list of document's page_content. list[str]
+            metadatas: list of document's infomation. list[dict[str]]
+                [{
+                    'source': 段落來源檔名(str),
+                    'page': 段落所在頁碼(int) # 只有 pdf 才有
+                    }, {}, ...
+                ]
         """
         docs = self.vectordb.similarity_search(query, k=n_results, filter=where)
-        # docs = self.vectordb.max_marginal_relevance_search(query, k=n_results, filter=where)
-        return docs 
+        contents, metadatas = [], []
+        for doc in docs:
+            contents.append(doc.page_content)
+            metadatas.append(doc.metadata)
+        return contents, metadatas
 
     # 刪除 _collection 裡的資料
-    def delete(self, where: dict[str]):
-        """Args example:
-        where =
-            (single condition)   {"source": "pdf-1"}
-            (multiple condition) {"$or": [{"source": "pdf-1"}, {"source": "pdf-4"}]}
+    def delete(self, where):
+        """
+        Args example:
+            where =
+                (single condition)   {"source": "pdf-1"}
+                (multiple condition) {"$or": [{"source": "pdf-1"}, {"source": "pdf-4"}]}
         """
         self.vectordb._collection.delete(where=where)
 
-    # 列印當前 collection 裡的 document 數量
-    def count(self) -> int:
+    # 回傳當前 collection 裡的 document 筆數
+    def count(self):
         return self.vectordb._collection.count()
